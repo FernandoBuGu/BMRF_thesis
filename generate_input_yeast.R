0e5249683e60e4a2dcf79b77d2e6e374c2584f9c
Fernando Bueno Gutiérrez <fernando.buenogutierrez@wur.nl>
2017-05-03 08:56:25 +0200

    Files with comments

--- generate_input_yeast.R
+++ generate_input_yeast.R
@@ -11,10 +11,8 @@ install.packages<-c("plyr","reshape","gsubfn","AUC","parallel","pbapply")
 
 #Combine coexpression networks
 filenames <- list.files(path="path_to_txt_files", full.names=TRUE)
-library(plyr)
+library(plyr,reshape)
 import.list <- llply(filenames, read.table)
-u<-sapply(import.list,dim)
-library(reshape)
 data <- merge_recurse(import.list)
 data[3]<-NULL
 dat.sort = t(apply(data, 1, sort))
@@ -23,8 +21,8 @@ data$V3<-1
 write.table(data,file="coexpression_all.txt",sep="\t", col.names = F, row.names = F, quote = F)
 
 #Obtain Uniprot codes to extract domains (only for labels that are in the network)
-expre = read.table("coexpression_all.txt",sep="\t")
-all_labels <- unique(c(as.character(expre[,1]),as.character(expre[,2])))	#domain and GO files should not contain any label that is not in this vector
+expre = read.table("coexpression_all.txt",sep="\t")		#	CHANGE THIS FOR SUBSETS
+all_labels <- unique(c(as.character(expre[,1]),as.character(expre[,2])))	#domain and GO files should contain only labels in this vector
 DF<-as.data.frame(rep(1,length(all_labels)))
 DF$label<-all_labels
 DF[1]<-NULL
@@ -37,7 +35,7 @@ write.table(DF,file="DF",sep="\t", col.names = F, row.names = F, quote = F)
 write.table(Codes_labels_inData,file="Codes_labels_inData",sep="\t", col.names = F, row.names = F, quote = F)
 write.table(expre,file="big_topless.tsv",sep="\t", col.names = F, row.names = F, quote = F)
 
-#~ python create_data_frame.py Codes_labels_inData			This takes around 30 minutes
+#~ python create_df_domains.py Codes_labels_inData			This takes around 30 minutes
 
 #python output -> domain file
 domains = read.table("domains",sep=",")
